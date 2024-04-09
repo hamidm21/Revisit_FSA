79a7b979e20f77aadd5b63da3deefcb8133dc5bb
Hamid Moradi <hordimad21@gmail.com>
2024-04-09 11:24:28 +0330

    added triple barrier labeling

--- util/labeling.py
+++ util/labeling.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from datetime import timedelta
 
 def label_ROC(df, window_size):
     """
@@ -23,6 +24,19 @@ def label_ROC(df, window_size):
     labels = df.rolling(window_size).apply(roc_label, raw=False)
 
     return labels
+
+def triple_barrier_labeling(sdf, volatility_period=2, upper_barrier_factor=1, lower_barrier_factor=1, vertical_barrier_timedelta=timedelta(days=1)):
+    df = sdf.copy()
+    # Define your barriers
+    df['upper_barrier'] = df['close'] + df['close'].rolling(volatility_period).std() * upper_barrier_factor
+    df['lower_barrier'] = df['close'] - df['close'].rolling(volatility_period).std() * lower_barrier_factor
+    df['vertical_barrier'] = df.index + vertical_barrier_timedelta
+
+    # Label the observations
+    df['label'] = np.where(df['high'].shift(-1) > df['upper_barrier'], 2,
+                           np.where(df['low'].shift(-1) < df['lower_barrier'], 0, 1))
+    
+    return df
 
 def get_label_probabilities(df, features):
     # Define and train your model
